46df6df02741657fdca4d920e7a5a03e01ad3235
arissa97 <insyirarahmat@gmail.com>
2021-09-02 17:38:30 +0800

    dht

--- mylib/publishdht.py
+++ mylib/publishdht.py
@@ -6,10 +6,28 @@ from paho.mqtt import client as mqtt_client
 import board
 import adafruit_dht
 
+import sys
+sys.path.insert(1,'./i2clibraries')
+from i2c_hmc5883l import *
+from i2c_adxl345 import *
+from i2c_itg3205 import *
+from time import *
+import time
+
 dhtDevice = adafruit_dht.DHT22(board.D14)
 temperature_c = dhtDevice.temperature
 temperature_f = temperature_c * (9/5) + 32
 humidity = dhtDevice.humidity
+
+hmc5883l = i2c_hmc5883l(1)
+#i = 1
+
+hmc5883l.setContinuousMode ()
+hmc5883l.setDeclination (2,4)
+
+adxl345 = i2c_adxl345(1)
+
+itg3205 = i2c_itg3205(1)
 
 broker = '192.168.0.105' #broker.emqx.io 192.168.0.106
 port = 1883
@@ -37,11 +55,24 @@ def publish(client):
     #msg_count = 0
     while True:
         temperature_c = dhtDevice.temperature
+        (itgready, dataready) = itg3205.getInterruptStatus ()
+        (xgy, ygy, zgy) = itg3205.getDegPerSecAxes ()
+        (xacc, yacc, zacc) = adxl345.getAxes()
+        (xcom, ycom, zcom) = hmc5883l.getAxes()
         msg1 =temperature_c
         msg2 =temperature_f
         msg3 =humidity
         msg4= ","
-        msg = str (msg1) + msg4 +str(msg2) + msg4 +str (msg3)
+        msg5= xacc
+        msg6= yacc
+        msg7= zacc
+        msg8= xcom
+        msg9= ycom
+        msg10= zcom
+        msg11= xgy
+        msg12= ygy
+        msg13= zgy
+        msg = str (msg1) + msg4 +str(msg2) + msg4 +str (msg3) +msg4 + str(msg5) +msg4 + str(msg6)+msg4 + str(msg7)+msg4 + str(msg8)+msg4 + str(msg9)+msg4 + str(msg10)+msg4 + str(msg11)+msg4 + str(msg12)+msg4 + str(msg13)
         result = client.publish(topic, msg)
         # result: [0, 1]
         status = result[0]
